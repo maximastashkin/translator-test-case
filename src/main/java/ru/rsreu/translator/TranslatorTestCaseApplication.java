1b37c62b38b2670f803019ccbaa260e34d7a52ed
maximastashkin <maksim.astash@gmail.com>
2022-09-10 19:44:42 +0300

    Init project

---
+++ src/main/java/ru/rsreu/translator/TranslatorTestCaseApplication.java
@@ -0,0 +1,13 @@
+package ru.rsreu.translator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslatorTestCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranslatorTestCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/rsreu/translator/TranslatorTestCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rsreu.translator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslatorTestCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
